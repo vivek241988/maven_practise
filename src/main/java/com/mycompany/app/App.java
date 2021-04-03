bef9e4cbaec58871dc53caf558ec8b08b58fbc35
vivek241988 <71559436+vivek241988@users.noreply.github.com>
2021-04-03 13:36:45 +0530

    updated app java file

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello this is maven demo and practise!" );
+        System.out.println( "Hello this is maven class!" );
     }
 }
